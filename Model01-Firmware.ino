7fc435a6a9e17db90cab5a0aa8f40fba1db58714
Michael McClimon <michael@mcclimon.org>
2019-07-20 09:06:58 -0400

    Port over layer 1 to be like my ergodox

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -174,6 +174,22 @@ KEYMAPS(
 
 #if defined (PRIMARY_KEYMAP_QWERTY)
   [PRIMARY] = KEYMAP_STACKED
+  (___             , Key_1         , Key_2                , Key_3       , Key_4 , Key_5 , Key_LEDEffectNext ,
+   Key_Tab         , Key_Q         , Key_W                , Key_E       , Key_R , Key_T , Key_Backtick      ,
+   Key_LeftControl , Key_A         , Key_S                , Key_D       , Key_F , Key_G ,
+   Key_LeftShift   , Key_Z         , Key_X                , Key_C       , Key_V , Key_B , Key_Tab           ,
+   Key_LeftControl , Key_Backspace , ShiftToLayer(SYMBOL) , Key_LeftGui ,
+   ShiftToLayer(FUNCTION),
+
+   M(MACRO_ANY) , Key_6     , Key_7        , Key_8            , Key_9      , Key_0         , Key_Minus      ,
+   Key_Enter    , Key_Y     , Key_U        , Key_I            , Key_O      , Key_P         , Key_Backslash  ,
+                  Key_H     , Key_J        , Key_K            , Key_L      , Key_Semicolon , Key_Quote      ,
+   Key_Equals   , Key_N     , Key_M        , Key_Comma        , Key_Period , Key_Slash     , Key_RightShift ,
+   Key_RightAlt , Key_Enter , Key_Spacebar , Key_RightControl ,
+   ShiftToLayer(FUNCTION)),
+
+/* orig....
+  [PRIMARY] = KEYMAP_STACKED
   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
@@ -187,6 +203,7 @@ KEYMAPS(
    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
+*/
 
 #elif defined (PRIMARY_KEYMAP_DVORAK)
 
